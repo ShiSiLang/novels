474fec9ed62761a79d0362190b4b22509b972ea4
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2023-01-04 09:44:13 -0600

    Updated

--- index.js
+++ index.js
@@ -1,63 +1,77 @@
 const express = require("express");
-const novel = require('./starOfLibby');
+const novel = require("./starOfLibby");
 const app = express();
-const fs = require('fs');
-const axios = require('axios');
+const fs = require("fs");
+const axios = require("axios");
 
 const dir = (text) => `${__dirname}/html/${text}.html`;
 const link = (input) => `https://novels-production.up.railway.app/${input}`;
 
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
-app.get('/', (_, res) => res.redirect(link('home')));
-app.get('/home', async (_, res) => {
-    const fetched = await axios.get(`https://api.countapi.xyz/hit/star-of-libby.shinpitekita.repl.co/${process.env.count}`);
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.get("/", (_, res) => res.redirect(link("home")));
+app.get("/home", async (_, res) => {
+  const fetched = await axios.get(
+    `https://api.countapi.xyz/hit/star-of-libby.shinpitekita.repl.co/${process.env.count}`
+  );
   let file = fs.readFileSync("./html/home.html", {
-		encoding: "utf8"
-	})
-  file = file.replace("$$visits$$", `${fetched.data.value}`)
-  res.send(file)
-})
-app.get('/read/:chapter', async (req, res) => {
+    encoding: "utf8",
+  });
+  file = file.replace("$$visits$$", `${fetched.data.value}`);
+  res.send(file);
+});
+app.get("/read/:chapter", async (req, res) => {
   let chapter = Number(req.params.chapter) || 1;
   let file = fs.readFileSync("./html/sol.html", {
-		encoding: "utf8"
-	})
-  file = file.replace("$$change$$", `'https://novels-production.up.railway.app/novel/${Number(chapter) - 1}'`)
-  file = file.replace("$$change2$$", `${Number(chapter) + 1}`)
-  file = file.replace("$$change3$$", `${Number(chapter) - 1}`)
-  file = file.replace("$$change4$$", novel[chapter - 1]?.chapter || "Chapter Not Found.")
-  res.send(file)
-})
-app.get('/novel/:chapter', (req, res) => {
-  let chapter = Number(req.params.chapter) 
-	res.send(novel[chapter]);
+    encoding: "utf8",
+  });
+  file = file.replace(
+    "$$change$$",
+    `'https://novels-production.up.railway.app/novel/${Number(chapter) - 1}'`
+  );
+  file = file.replace("$$change2$$", `${Number(chapter) + 1}`);
+  file = file.replace("$$change3$$", `${Number(chapter) - 1}`);
+  file = file.replace(
+    "$$change4$$",
+    novel[chapter - 1]?.chapter || "Chapter Not Found."
+  );
+  file = file.replace(
+    "$$thumbnail$$",
+    novel[chapter - 1]?.thumbnail || "https://i.imgur.com/lGLKiVd.png"
+  );
+  res.send(file);
 });
-app.get('/discord', (_, res) => res.redirect('https://discord.gg/j3YamACwPu'));
+app.get("/novel/:chapter", (req, res) => {
+  let chapter = Number(req.params.chapter);
+  res.send(novel[chapter]);
+});
+app.get("/discord", (_, res) => res.redirect("https://discord.gg/j3YamACwPu"));
 
-app.use((_, res) => res.status(404).sendFile(dir('error')));
+app.use((_, res) => res.status(404).sendFile(dir("error")));
 
 app.listen(process.env.PORT || 80, () => {
-	console.log('Server Started');
+  console.log("Server Started");
 });
 
-console.log(__dirname)
+console.log(__dirname);
 
 process.on("unhandledRejection", (reason, p) => {
-	console.log(" [antiCrash] :: Unhandled Rejection/Catch");
-	console.log(reason, p);
+  console.log(" [antiCrash] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
 });
 process.on("uncaughtException", (err, origin) => {
-	console.log(" [antiCrash] :: Uncaught Exception/Catch");
-	console.log(err, origin);
+  console.log(" [antiCrash] :: Uncaught Exception/Catch");
+  console.log(err, origin);
 });
 process.on("uncaughtExceptionMonitor", (err, origin) => {
-	console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
-	console.log(err, origin);
+  console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
 });
 process.on("multipleResolves", (type, promise, reason) => {
-	console.log(" [antiCrash] :: Multiple Resolves"); 
-	console.log(type, promise, reason);
+  console.log(" [antiCrash] :: Multiple Resolves");
+  console.log(type, promise, reason);
 });
