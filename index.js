e0beb4e6dba9be9f34156fc90fcca28c6739f785
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-03-16 21:16:55 -0500

    test

--- index.js
+++ index.js
@@ -323,7 +323,7 @@ app.post("/follow", async (req, res) => {
 });
 
 app.post("/comment", async (req, res) => {
-  let html = req.body;
+  let html = req.body.data;
   console.log(html)
   let profileArray = await comments.findOne({ password: "ShinpiIsCool" });
   let user = profileArray.profiles.find(
