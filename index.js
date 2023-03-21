6021a752d122065aaf0d819cf0eb5b854fab5980
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-03-21 09:58:16 -0500

    Update

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 const fs = require("fs");
 const axios = require("axios");
 const mongoose = require("mongoose");
+const profileShema = require("./models/profiles");
+const bookShema = require("./models/book");
 
 const dir = (text) => `${__dirname}/html/${text}.html`;
 const link = (input) => `https://novels-production.up.railway.app/${input}`;
@@ -17,45 +19,35 @@ app.use(
 );
 app.get("/", (_, res) => res.redirect(link("home")));
 
-app.get("/home", async (_, res) => {
-  const fetched = await axios.get(
-    `https://api.countapi.xyz/hit/star-of-libby.shinpitekita.repl.co/${process.env.count}`
-  );
-  let file = fs.readFileSync("./html/home.html", {
-    encoding: "utf8",
-  });
-  file = file.replace("$$visits$$", `${fetched.data.value}`);
-  res.send(file);
-});
-app.get("/sign-up", (_, res) => res.sendFile(dir("sign-up")));
+app.get("/home", async (_, res) => res.sendFile(dir("home")));
 
-app.get("/explore", async (req, res) => res.sendFile(dir("explore")));
+app.get("/sign-up", (_, res) => res.sendFile(dir("sign-up")));
 
 app.get("/discord", (_, res) => res.redirect("https://discord.gg/j3YamACwPu"));
 
-app.get("/publish", (_, res) => res.sendFile(dir("explore")));
+app.get("/publish", (_, res) => res.sendFile(dir("publish")));
 
-/*
-Book Data:
-Name
-Desc
-Author
-chapters: Array
-*/
+app.get("/explore", async (req, res) => {
+  //get data
+  res.sendFile(dir("explore"));
+});
 
-app.get("/explore/:book", async (req, res) => {
-  /*
-  if(!foundBook)return res.sendFile(dir("error"))
-  */
+app.get("/explore/:bookName", async (req, res) => {
+  let bookName = req.params.bookName.toLowerCase();
+  let book = await bookShema.findOne({ name: bookName });
+
+  if (!book) return res.sendFile(dir("error"));
+
   let file = fs.readFileSync("./html/book.html", {
     encoding: "utf8",
   });
-  file = file.replaceAll("$$name$$", `Star of Libby, Adiós Hogar Mío`);
-  file = file.replaceAll("$$desc$$", `What happens when a tiger owns a gang?`);
-  file = file.replaceAll("$$author$$", `Lonely Ball`);
+  file = file.replaceAll("$$name$$", book.name);
+  file = file.replaceAll("$$desc$$", book.description);
+  file = file.replaceAll("$$author$$", book.author);
+  file = file.replaceAll("$$icon$$", book.icon);
   file = file.replaceAll(
     "$$novel$$",
-    "'https://novels-production.up.railway.app/data/novel'"
+    `'https://novels-production.up.railway.app/data/book/${book.name}'`
   );
 
   res.send(file);
@@ -82,7 +74,7 @@ app.get("/read/:chapter", async (req, res) => {
     novel[chapter - 1]?.thumbnail || "https://i.imgur.com/lGLKiVd.png"
   );
   res.send(file);
-});
+}); //make sure to change this after publishing StarofLibby on the data
 
 app.get("/novel/:chapter", async (req, res) => {
   let chapter = Number(req.params.chapter);
@@ -92,10 +84,67 @@ app.get("/novel/:chapter", async (req, res) => {
     ...novel[chapter],
   };
   res.send(newObj);
+}); //make sure to delete this after publishing StarofLibby on the data
+
+app.get("/data/:type/:other", async (req, res) => {
+  let type = req.params.type.toLowerCase();
+
+  if (type === "profiles") {
+    let data = await profileShema.find().sort({ username: 1 });
+    console.log(data);
+    let newObj = data.map((v) => v.username);
+    res.send(newObj);
+  }
+  if (type === "books") {
+    let data = await bookShema.find().sort({ name: 1 });
+    res.send(data);
+  }
+  if (type === "book") {
+    let bookName = req.params.other.toLowerCase();
+    if (!bookName)
+      return res.status(400).json({ error: `Please provide a book name` });
+    let book = await bookShema.findOne({ name: bookName });
+    console.log(book);
+    res.send(book);
+  }
+}); //very important
+
+app.get("/profile/:username", async (req, res) => {
+  let username = req.params.username.toLowerCase();
+
+  let userData = await profileShema.findOne((v) => v.username === username);
+  console.log(userData);
+
+  if (!userData) return res.sendFile(dir("error"));
+
+  let file = fs.readFileSync("./html/profile.html", {
+    encoding: "utf8",
+  });
+
+  let data = await bookShema.find().sort({ name: 1 });
+
+  let comments = data.filter(
+    (v) => v.chapters.comments.username === userData.username
+  );
+
+  console.log(comments);
+
+  file = file.replaceAll("$$username$$", userData.username);
+  file = file.replaceAll("$$avatar$$", userData.icon);
+  file = file.replaceAll("$$followers$$", userData.followers);
+  file = file.replaceAll("$$comments$$", comments.length);
+  file = file.replace("$$discord$$", userData.discord || "#");
+  file = file.replace("$$twitter$$", userData.twitter || "#");
+  file = file.replace("$$date$$", userData.date);
+  file = file.replace(
+    "$$profiles$$",
+    `'https://novels-production.up.railway.app/data/profiles'`
+  );
+  res.send(file);
 });
 
 app.post("/sign-up", async (req, res) => {
-  let html = req.body;
+  let html = req.body.data;
   if (html.dp !== process.env.profiles)
     return res.status(400).json({ error: `Incorrect password!` });
 
@@ -114,76 +163,31 @@ app.post("/sign-up", async (req, res) => {
   let newdate =
     date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
 
-  await comments.findOneAndUpdate(
-    { password: "ShinpiIsCool" },
-    {
-      $push: {
-        profiles: {
-          username: html.uname.replace(/</g, "&lt;"),
-          icon: image,
-          password: html.psw.replace(/</g, "&lt;"),
-          date: `${newdate}`,
-          followers: 0,
-          following: [],
-          discord: null,
-          twitter: null,
-        },
-      },
-    }
-  );
+  let data = await profileShema.find().sort({ username: 1 });
+  console.log(data);
 
-  return res
-    .status(200)
-    .json({ success: `${html.uname.replace(/</g, "&lt;")} added!` });
-});
+  if (data.filter((v) => v.username === html.uname.replace(/</g, "&lt;")))
+    return res.status(400).json({ error: `That username is already taken.` });
 
-app.get("/data/:name", async (req, res) => {
-  let type = req.params.name.toLowerCase();
+  let newProfile = new profileShema({
+    username: html.uname.replace(/</g, "&lt;"),
+    password: html.psw.replace(/</g, "&lt;"),
+    icon: image,
+    date: `${newdate}`,
+    followers: 0,
+    discord: null,
+    twitter: null,
+    author: false,
+  }).save();
 
-  if (type === "profiles") {
-    let db = await comments.findOne({ password: "ShinpiIsCool" });
-    let newObj = db.profiles.map((v) => v.username);
-    res.send(newObj);
-  }
-  if (type === "novel") {
-    res.send(novel);
-  }
-});
-
-app.get("/profile/:username", async (req, res) => {
-  let username = req.params.username.toLowerCase();
-  let db = await comments.findOne({ password: "ShinpiIsCool" });
-  let user = db.profiles.find(
-    (v) => v.username.toLowerCase() === username.replace(/</g, "&lt;")
-  );
-  if (!user)
-    return res.status(400).json({ error: `Account not found, try again.` });
-
-  let file = fs.readFileSync("./html/profile.html", {
-    encoding: "utf8",
-  });
-  file = file.replaceAll("$$username$$", user.username);
-  file = file.replaceAll("$$avatar$$", user.icon);
-  file = file.replaceAll("$$followers$$", user?.followers || 0);
-  file = file.replaceAll(
-    "$$comments$$",
-    db.comments?.filter((v) => v.username === user.username).length || 0
-  );
-  file = file.replace("$$discord$$", user?.discord || "#");
-  file = file.replace("$$twitter$$", user?.twitter || "#");
-  file = file.replaceAll("$$avatar$$", user.icon);
-  file = file.replace("$$date$$", user?.date || "1/13/2023");
-  file = file.replace(
-    "$$profiles$$",
-    `'https://novels-production.up.railway.app/data/profiles'`
-  );
-  res.send(file);
+  return res.status(200).json({ success: `${newProfile.username} added!` });
 });
 
 app.post("/edit", async (req, res) => {
+  let data = await profileShema.find().sort({ username: 1 });
+  console.log(data);
   let html = req.body.data;
-  let profileArray = await comments.findOne({ password: "ShinpiIsCool" });
-  let user = profileArray.profiles.find(
+  let user = data.find(
     (v) =>
       v.password === html.psw.replace(/</g, "&lt;") &&
       v.username === html.uname.replace(/</g, "&lt;")
@@ -231,82 +235,64 @@ app.post("/edit", async (req, res) => {
       .status(400)
       .json({ error: `Please make sure the twitter link is a valid URL.` });
 
-  let objIndex = profileArray.profiles.findIndex(
+  let objIndex = data.findIndex(
     (v) =>
       v.password === html.psw.replace(/</g, "&lt;") &&
       v.username === html.uname.replace(/</g, "&lt;")
   );
 
-  profileArray.profiles[objIndex] = params;
-
-  await comments.findOneAndUpdate(
-    {
-      password: "ShinpiIsCool",
-    },
-    profileArray
-  );
+  data[objIndex] = params;
+  data.save();
 
   return res.status(200).json({ success: `Profile successfully edited.` });
 });
 
 app.post("/follow", async (req, res) => {
+  let data = await profileShema.find().sort({ username: 1 });
+  console.log(data);
   let html = req.body.data;
-  let profileArray = await comments.findOne({ password: "ShinpiIsCool" });
-  let user = profileArray.profiles.find(
+  let user = data.find(
     (v) =>
       v.password === html.psw.replace(/</g, "&lt;") &&
       v.username === html.uname.replace(/</g, "&lt;")
   );
+
   if (!user)
     return res.status(400).json({ error: `Incorrect username or password!` });
 
   if (html.uname === html.follow)
     return res.status(400).json({ error: `You cannot follow yourself!` });
 
-  let user2 = profileArray.profiles.find((v) => v.username === html.follow);
+  let user2 = data.find((v) => v.username === html.follow);
 
   if (user.following.includes(html.follow)) {
-    let objIndex = profileArray.profiles.findIndex(
+    let objIndex = data.findIndex(
       (v) =>
         v.password === html.psw.replace(/</g, "&lt;") &&
         v.username === html.uname.replace(/</g, "&lt;")
     );
-    let objIndex2 = profileArray.profiles.findIndex(
-      (v) => v.username === html.follow
-    );
+    let objIndex2 = data.findIndex((v) => v.username === html.follow);
 
     if (objIndex > -1 && objIndex2 > -1) {
-      profileArray.profiles[objIndex].following.splice(index, 1);
-      profileArray.profiles[objIndex2].followers -= 1;
-      await comments.findOneAndUpdate(
-        {
-          password: "ShinpiIsCool",
-        },
-        profileArray
-      );
+      data[objIndex].following.splice(index, 1);
+      data[objIndex2].followers -= 1;
+      data.save();
       return res
         .status(200)
         .json({ success: `Successfully unfollowed ${html.follow}!` });
     }
   } else {
-    let objIndex = profileArray.profiles.findIndex(
+    let objIndex = data.findIndex(
       (v) =>
         v.password === html.psw.replace(/</g, "&lt;") &&
         v.username === html.uname.replace(/</g, "&lt;")
     );
-    let objIndex2 = profileArray.profiles.findIndex(
-      (v) => v.username === html.follow
-    );
+    let objIndex2 = data.findIndex((v) => v.username === html.follow);
 
     if (objIndex > -1 && objIndex2 > -1) {
-      profileArray.profiles[objIndex].following.push(html.follow);
-      profileArray.profiles[objIndex2].followers += 1;
-      await comments.findOneAndUpdate(
-        {
-          password: "ShinpiIsCool",
-        },
-        profileArray
-      );
+      data[objIndex].following.push(html.follow);
+      data[objIndex2].followers += 1;
+      data.save();
       return res
         .status(200)
         .json({ success: `Successfully followed ${html.follow}!` });
@@ -315,9 +301,20 @@ app.post("/follow", async (req, res) => {
 });
 
 app.post("/comment", async (req, res) => {
-  let html = req.body.data;
-  let profileArray = await comments.findOne({ password: "ShinpiIsCool" });
-  let user = profileArray.profiles.find(
+  return res
+    .status(400)
+    .json({ error: `Commenting is disabled at the moment` });
+
+  let data = await profileShema.find().sort({ username: 1 });
+  console.log(data);
+
+  let html = req.body.data; //bookName, index
+  let bookData = await bookShema.findOne({ name: html.bookName });
+
+  if (!bookData)
+    return res.status(400).json({ error: `Something went wrong.` });
+
+  let user = data.find(
     (v) =>
       v.password === html.psw.replace(/</g, "&lt;") &&
       v.username === html.uname.replace(/</g, "&lt;")
@@ -329,26 +326,20 @@ app.post("/comment", async (req, res) => {
   let newdate =
     date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
 
-  await comments.findOneAndUpdate(
-    {
-      password: "ShinpiIsCool",
-    },
-    {
-      $push: {
-        comments: {
-          chapter: Number(html.chapter),
-          comment: `${html.comment.replace(/</g, "&lt;")}`,
-          username: `${user.username}`,
-          icon: `${user.icon}`,
-          date: `${newdate}`,
-        },
-      },
-    }
-  );
+  if (!bookData.chapters[html.index])
+    return res.status(400).json({ error: `Something went wrong.` });
 
-  return res
-    .status(200)
-    .json({ success: `Comment sent to chapter ${html.chapter}` });
+  bookData.chapters[html.index].push({
+    comment: html.comment.replace(/</g, "&lt;"),
+    username: user.username,
+    date: newdate,
+  });
+
+  console.log(bookData.chapters[html.index]);
+
+  bookData.save();
+
+  return res.status(200).json({ success: `Comment Posted!` });
 });
 
 app.use((_, res) => res.status(404).sendFile(dir("error")));
--- models/book.js
+++ models/book.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 /* chapters: { 
+    name: String,
+    intro: String,
+    credits: String,
+    chapterName: String,
+    timer: String,
+    thumbnail: ImageLink,
     type: image/novel, 
     content: image/String, 
-    comments: { username, comment, chapter, date } 
+    comments: { username, comment, date } 
 }*/
 const bookSchema = mongoose.Schema({
   name: String,
   description: String,
+  icon: String,
   author: String,
   chapters: Array,
 });
