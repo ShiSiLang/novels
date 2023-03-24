68a5316a02ac28c664f49dca88f90e1ae6eb9893
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-03-24 14:42:54 -0500

    Update review.js

--- models/review.js
+++ models/review.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const reviewSchema = mongoose.Schema({
   bookName: String,
   bookAuthor: String,
+  bookDescription: String,
+  bookIcon: String,
   reviewID: String,
 });
 
