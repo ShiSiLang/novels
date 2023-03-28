646089b3205e702c819d6326b39dcd050f410811
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-03-28 12:11:32 -0500

    Updated rating system

--- models/book.js
+++ models/book.js
@@ -15,6 +15,13 @@ const bookSchema = mongoose.Schema({
   icon: String,
   author: String,
   chapters: Array,
+  followers: Array,// It will only store usernames
+  1: Number, //  the key is the weight of that star level
+  2: Number,
+  3: Number,
+  4: Number,
+  5: Number,
+  default: { 1: 1, 2: 1, 3: 1, 4: 1, 5: 1 },
 });
 
 module.exports = mongoose.model("book", bookSchema);
