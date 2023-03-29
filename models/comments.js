ab0fb2f32db8b0211fdf396115666df859e5ad56
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-03-28 20:59:33 -0500

    Update comment on publishing

--- models/comments.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const comment = mongoose.Schema({
-  password: { type: String, required: true },
-  comments: Array,
-  profiles: Array,
-});
-
-module.exports = mongoose.model("website", comment);
