9ff1c16692922b8ceca6b10add6e9b7fac053381
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-04-17 07:08:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -126,7 +126,8 @@ app.get("/data/:type/:other", async (req, res) => {
   }
   if (type === "latest") {
     let getSystem = await system.find({ id: "6427a45e2d7d901440fc43cf" });
-    res.send(getSystem.latestChapters);
+    res.send(getSystem);
+//latestChapters
   }
   if (type === "books") {
     let data = await bookShema.find().sort({ name: 1 });
