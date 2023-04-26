22ecca029890b21cccf819b0e52900a01db4d002
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-04-25 19:34:07 -0500

    update

--- index.js
+++ index.js
@@ -423,7 +423,7 @@ app.post("/sign-up", async (req, res) => {
       .status(200)
       .json({ success: `${html.uname.replace(/</g, "&lt;")} added!` });
   } catch (error) {
-    console.error(error);
+    console.error(error.message);
     return res.status(400).send({ error: error.message });
   }
 });
