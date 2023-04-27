48f31b36e49037b3f0d13d0f450527cab3736d0d
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-04-27 14:06:30 -0500

    Update

--- models/system.js
+++ models/system.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const system = mongoose.Schema({
   registeredBots: Array,
   latestChapters: Array,
+  readingNow: Array,
 });
 
 module.exports = mongoose.model("system", system);
