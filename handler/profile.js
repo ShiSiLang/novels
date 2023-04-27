9361c1e3725f4e910522585d6feaca891b9f71f8
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-04-27 06:47:11 -0500

    Update profile.js

--- handler/profile.js
+++ handler/profile.js
@@ -5,9 +5,9 @@ module.exports = {
   get: true,
   run: async (req, res) => {
     let id = req.params.id;
-    let data = await profileShema.findOne({ id: id });
+    let user = await profileShema.findOne({ id: id });
 
-    if (!data) return res.sendFile(dir("error"));
+    if (!user) return res.sendFile(dir("error"));
 
     let file = fs.readFileSync("./html/profile.html", {
       encoding: "utf8",
@@ -21,7 +21,7 @@ module.exports = {
           v.chapters.filter((e) => {
             return (
               e.comments.filter((c) => {
-                return c.id === data.id;
+                return c.id === user.id;
               }).length > 0
             );
           }).length > 0
@@ -30,26 +30,26 @@ module.exports = {
       .flatMap((v) => {
         return v.chapters.flatMap((e) => {
           return e.comments.filter((c) => {
-            return c.id === data.id;
+            return c.id === user.id;
           });
         });
       });
 
-    file = file.replaceAll("$$username$$", data.username);
-    file = file.replace("$$banner$$", data.banner);
+    file = file.replaceAll("$$username$$", user.username);
+    file = file.replace("$$banner$$", user.banner);
     file = file.replaceAll(
       "$$avatar$$",
-      `https://cdn.discordapp.com/avatars/${id}/${data.avatar}.png`
+      `https://cdn.discordapp.com/avatars/${id}/${user.avatar}.png`
     );
-    file = file.replaceAll("$$followers$$", data.followers);
+    file = file.replaceAll("$$followers$$", user.followers);
     file = file.replaceAll("$$comments$$", comments.length);
-    file = file.replace("$$discord$$", data.discord || "#");
-    file = file.replace("$$twitter$$", data.twitter || "#");
+    file = file.replace("$$discord$$", user.discord || "#");
+    file = file.replace("$$twitter$$", user.twitter || "#");
     file = file.replace(
       "$$bio$$",
-      userData?.bio || `Welcome to ${data.username}'s profile!`
+      user.bio || `Welcome to ${data.username}'s profile!`
     );
-    file = file.replace("$$date$$", data.date);
+    file = file.replace("$$date$$", user.date);
     res.send(file);
   },
 };
