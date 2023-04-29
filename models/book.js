e553d7ec9681caf6c8b664aa64bf6342db36bba9
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-04-29 12:58:04 -0500

    Update

--- models/book.js
+++ models/book.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const bookSchema = mongoose.Schema({
-  name: String,
-  description: String,
-  icon: Buffer,
-  author: String,
-  chapters: [
-    {
-      name: String,
-      intro: String,
-      credits: String,
-      thumbnail: Buffer,
-      type: String, //Novel/Manga/Webtoon
-      images: [Buffer], // Array of binary image data for manga/webtoon
-      novel: String, // String for novels
-      comments: [
-        {
-          username: String,
-          userID: String,
-          comment: String,
-          date: Date,
-        },
-      ],
-    },
-  ],
-  followers: Array, // Array of ids
-  tags: Array,
-  updated: String,
-  status: String,
-  published: String,
-  views: { type: Number, default: 0 },
-}, { typeKey: '$type' });
+const bookSchema = mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    icon: Buffer,
+    author: String,
+    chapters: [
+      {
+        name: String,
+        intro: String,
+        credits: String,
+        thumbnail: Buffer,
+        type: String, //Novel/Manga/Webtoon
+        images: [Buffer], // Array of binary image data for manga/webtoon
+        novel: String, // String for novels
+        comments: [
+          {
+            username: String,
+            userID: String,
+            comment: String,
+            date: Date,
+          },
+        ],
+      },
+    ],
+    followers: Array, // Array of ids
+    tags: Array,
+    updated: String,
+    status: String,
+    published: String,
+    views: { type: Number, default: 0 },
+  },
+  { typeKey: "$type" }
+);
 
 module.exports = mongoose.model("book", bookSchema);
