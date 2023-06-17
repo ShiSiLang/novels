432549e50d1ab91024d496045105fbf6cc1c7847
LonelyOrangeBall <82416698+loneorangeball@users.noreply.github.com>
2023-06-17 13:31:02 -0500

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 },
+limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB limit
 });
 
 module.exports = function (app) {
